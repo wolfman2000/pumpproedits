b637a1e03d2ea1381f5b5356e576b0b492051646
Jason Felds <jafelds@gmail.com>
2010-04-15 19:28:33 -0400

    Use SQLite3.

--- application/config/database.php
+++ application/config/database.php
@@ -37,11 +37,11 @@
 $active_group = "default";
 $active_record = TRUE;
 
-$db['default']['hostname'] = "localhost";
+$db['default']['hostname'] = "";
 $db['default']['username'] = "";
 $db['default']['password'] = "";
-$db['default']['database'] = "";
-$db['default']['dbdriver'] = "mysql";
+$db['default']['database'] = "sqlite:".APPPATH."db/pumpproedits.db";
+$db['default']['dbdriver'] = "pdo";
 $db['default']['dbprefix'] = "";
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
