b19c9e85fe2d730322e4c78afe7997e4b8289d01
Jason Felds <jafelds@gmail.com>
2010-04-20 10:36:00 -0400

    Working Edit Creator submission. The creator's features match what's on the live site now. Still, new things need to be added before this goes live.

--- application/controllers/create.php
+++ application/controllers/create.php
@@ -9,6 +9,7 @@ class Create extends Controller
     $this->load->model('ppe_song_song');
     $this->load->model('ppe_song_game');
     $this->load->model('ppe_user_power');
+    $this->load->model('ppe_user_user');
     $this->load->model('ppe_song_bpm');
     $this->load->model('ppe_song_stop');
     $this->load->model('ppe_edit_edit');
@@ -179,6 +180,73 @@ class Create extends Controller
     echo json_encode($ret);
 
   }
+  // Upload the edit directly to the website.
+  // Should I allow title changing, and risk overriding?
+  function upload()
+  {
+     if (!(isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+      strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest'))
+    {
+      return;
+    }
+    header("Content-Type: application/json");
+    $row = array();
+    $eid = $this->input->post('editID');
+    $row['id'] = $this->input->post('songID'); // must stay consistent.
+    $row['uid'] = $this->input->post('userID');
+    $row['title'] = $this->input->post('title');
+    $row['style'] = "pump-" . $this->input->post('style');
+    $row['diff'] = $this->input->post('diff');
+    $row['steps'] = array();
+    $row['steps'][] = $this->input->post('steps1');
+    $row['steps'][] = $this->input->post('steps2');
+    $row['jumps'] = array();
+    $row['jumps'][] = $this->input->post('jumps1');
+    $row['jumps'][] = $this->input->post('jumps2');
+    $row['holds'] = array();
+    $row['holds'][] = $this->input->post('holds1');
+    $row['holds'][] = $this->input->post('holds2');
+    $row['mines'] = array();
+    $row['mines'][] = $this->input->post('mines1');
+    $row['mines'][] = $this->input->post('mines2');
+    $row['trips'] = array();
+    $row['trips'][] = $this->input->post('trips1');
+    $row['trips'][] = $this->input->post('trips2');
+    $row['rolls'] = array();
+    $row['rolls'][] = $this->input->post('rolls1');
+    $row['rolls'][] = $this->input->post('rolls2');
+    $row['lifts'] = array();
+    $row['lifts'][] = $this->input->post('lifts1');
+    $row['lifts'][] = $this->input->post('lifts2');
+    $row['fakes'] = array();
+    $row['fakes'][] = $this->input->post('fakes1');
+    $row['fakes'][] = $this->input->post('fakes2');
+    
+    # Can't use <= on the below: what if it's null?
+    if (!($eid > 0)) # New edit
+    {
+      $eid = $this->ppe_edit_edit->addEdit($row);
+      $status = "New";
+    }
+    else
+    {
+      $this->ppe_edit_edit->updateEdit($eid, $row);
+      $status = "Updated";
+    }
+    $this->db->cache_delete_all();
+    $this->load->helper('twitter');
+    $twit = genEditMessage($row['uid'], $this->ppe_user_user->getUserByID($row['uid']), $status);
+    postTwitter($twit);
+    
+    $path = sprintf("%sdata/user_edits/edit_%06d.edit.gz", APPPATH, $eid);
+    $fp = gzopen($path, "w");
+    gzwrite($fp, $this->input->post('b64'));
+    gzclose($fp);
+    $ret = array();
+    $ret['result'] = "successful";
+    echo json_encode($ret);
+  }
+  
   // Download the edit created directly.
   function download()
   {
