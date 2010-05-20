0c8a177cf95cec4efa2f92451257d8e69a02e437
Jason Felds <jafelds@gmail.com>
2010-05-19 22:53:45 -0400

    No counter on localhost.

--- application/views/global/footer.php
+++ application/views/global/footer.php
@@ -3,6 +3,7 @@
 <footer>
 <p>This website is ©2009-2010 <a href="mailto:jafelds@gmail.com">Jason"Wolfman2000" Felds</a>.<br />
 This website works best in <a href="http://www.firefox.com">Firefox</a>.</p>
+<?php if (strpos("localhost", $_SERVER['SCRIPT_NAME']) !== false): ?>
 <!-- Site Meter -->
 <script type="text/javascript" src="http://s36.sitemeter.com/js/counter.js?site=s36pumpproedits">
 </script>
@@ -11,7 +12,7 @@ This website works best in <a href="http://www.firefox.com">Firefox</a>.</p>
 <img src="http://s36.sitemeter.com/meter.asp?site=s36pumpproedits" alt="Site Meter" border="0"/></a>
 </noscript>
 <!-- Copyright (c)2009 Site Meter -->
-
+<?php endif; ?>
 </footer>
 </body>
 </html>
