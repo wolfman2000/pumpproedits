158b2d4c72e253883b1c0f4eda8e2d045a9c16c1
Jason Felds <jafelds@gmail.com>
2010-05-19 21:04:16 -0400

    Another try at multi column CSS. May have to make it a table after all.

--- application/views/edits/counter.php
+++ application/views/edits/counter.php
@@ -1,6 +1,6 @@
 <section id="multiCol">
 <?php foreach ($query as $r): ?>
-<p><span><?php echo anchor(sprintf("/%s/%d", $what, $r->id), $r->core); ?></span>
-<?php echo $r->num_edits; ?></p>
+<p><?php echo anchor(sprintf("/%s/%d", $what, $r->id), $r->core); ?>
+<span><?php echo $r->num_edits; ?></span></p>
 <?php endforeach; ?>
 </section>
