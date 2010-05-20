480c6935e9fb1124419c0b7462afa39563035f02
Jason Felds <jafelds@gmail.com>
2010-05-19 21:07:12 -0400

    Use visitor tracker.

--- application/views/global/footer.php
+++ application/views/global/footer.php
@@ -1,6 +1,17 @@
 </article>
 <?php $this->load->view("global/nav_" . ($this->uri->segment(1) !== "create" ? "normal" : "create")); ?>
-<footer>This website is ©2009-2010 <a href="mailto:jafelds@gmail.com">Jason “Wolfman2000” Felds</a>.<br />
-This website works best in <a href="http://www.firefox.com">Firefox</a>.</footer>
+<footer>
+<p>This website is ©2009-2010 <a href="mailto:jafelds@gmail.com">Jason"Wolfman2000" Felds</a>.<br />
+This website works best in <a href="http://www.firefox.com">Firefox</a>.</p>
+<!-- Site Meter -->
+<script type="text/javascript" src="http://s36.sitemeter.com/js/counter.js?site=s36pumpproedits">
+</script>
+<noscript>
+<a href="http://s36.sitemeter.com/stats.asp?site=s36pumpproedits" target="_top">
+<img src="http://s36.sitemeter.com/meter.asp?site=s36pumpproedits" alt="Site Meter" border="0"/></a>
+</noscript>
+<!-- Copyright (c)2009 Site Meter -->
+
+</footer>
 </body>
 </html>
