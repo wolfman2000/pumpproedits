1b65ea7594807f176d1f73b7d9a914c8152314e2
Jason Felds <jafelds@gmail.com>
2010-07-26 15:07:31 -0400

    Missed a duplicate jQuery call.

--- application/views/chart/songs.php
+++ application/views/chart/songs.php
@@ -1,5 +1,5 @@
 <?php $this->load->view('global/header',
-  array('css' => 'css/chart.css', 'scripts' => array('/js/jquery-1.4.2.js', '/js/official.js'),
+  array('css' => 'css/chart.css', 'scripts' => array('/js/official.js'),
   'h2' => 'Official Chart Generator', 'title' => 'Official Chart Generator')); ?>
 <p>Select the song and difficulty you want to see a chart of.</p>
 
