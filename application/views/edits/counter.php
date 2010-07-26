c902244bdf80c145d95cdb9fc439ef9cb95b6ead
Jason Felds <jafelds@gmail.com>
2010-07-26 14:46:21 -0400

    Use data table for now. Give footer space.

--- application/views/edits/counter.php
+++ application/views/edits/counter.php
@@ -1,6 +1,18 @@
-<section id="multiCol">
-<?php foreach ($query as $r): ?>
-<p><?php echo anchor(sprintf("/%s/%d", $what, $r->id), $r->core); ?>
-<span><?php echo $r->num_edits; ?></span></p>
-<?php endforeach; ?>
-</section>
+<table id="multiCol">
+<thead><tr>
+<th><?php echo ucfirst($what); ?></th><th>Count</th>
+<th><?php echo ucfirst($what); ?></th><th>Count</th>
+</tr></thead>
+<tbody>
+<tr>
+<?php $counter = -1;
+foreach ($query as $r): ?>
+<td><?php echo anchor(sprintf("/%s%d", $what, $r->id), $r->core); ?></td>
+<td><?php echo $r->num_edits; ?></td>
+<?php if (++$counter % 2): ?>
+</tr>
+<tr>
+<?php endif; endforeach; ?>
+</tr>
+</tbody>
+</table>
