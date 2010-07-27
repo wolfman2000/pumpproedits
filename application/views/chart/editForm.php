4d036094c1fe9a2b2e5ec544a3117ef0323110ab
Jason Felds <jafelds@gmail.com>
2010-07-27 12:06:57 -0400

    Reorganize Chart Generator with 960 GS.

--- application/views/chart/editForm.php
+++ application/views/chart/editForm.php
@@ -1,8 +1,12 @@
 <?php echo form_open('chart/editProcess'); ?>
 <fieldset><legend>Select the edit to preview.</legend>
+<div class="clear"></div>
+<section id="errorCatch" class="grid_6">
 <?php echo validation_errors(); ?>
-
-<section id="edit">
+</section>
+<div class="clear"></div>
+<div class="grid_12">
+<section id="edit" class="grid_7 alpha">
 <p><label for="edits">Choose an edit</label></p>
 <p><select id="edits" name="edits" size="20">
 <?php $oname = "無"; # Start with no match. ?>
@@ -22,45 +26,76 @@ endif; ?>
 </optgroup>
 </select></p>
 </section>
-<section>
-<p><label for="kind">Noteskin</label></p>
-<p><select id="kind" name="kind">
-<option selected="selected" value="classic">Classic</option>
-<option value="rhythm">Rhythm</option>
-</select></p>
-<p><label for="red4">4th Note Color</label></p>
-<p><select id="red4" name="red4">
-<option value="0" selected="selected">Blue</option>
-<option value="1">Red</option>
-</select></p>
-<p><label for="speed">Speed Mod</label></p>
-<p><select id="speed" name="speed">
-<option value="1">1</option>
-<option value="2" selected="selected">2</option>
-<option value="3">3</option>
-<option value="4">4</option>
-<option value="6">6</option>
-<option value="8">8</option>
-</select></p>
-<p><label for="mpcol">Measure per column</label></p>
-<p><select id="mpcol" name="mpcol">
-<option value="4">4</option>
-<option value="6" selected="selected">6</option>
-<option value="8">8</option>
-<option value="12">12</option>
-<option value="16">16</option>
-</select></p>
-<p><label for="scale">Scale Factor</label></p>
-<p><select id="scale" name="scale">
-<option value="0.5">0.5</option>
-<option value="0.75">0.75</option>
-<option value="1" selected="selected">1</option>
-<option value="1.25">1.25</option>
-<option value="1.5">1.5</option>
-<option value="1.75">1.75</option>
-<option value="2">2</option>
-</select></p>
+<?php $options = array
+(
+  array
+  (
+    "for" => "kind", "label" => "Noteskin", "choices" => array
+    (
+      array("value" => "classic", "text" => "Classic", "selected" => true),
+      array("value" => "rhythm", "text" => "Rhythm", "selected" => false),
+    ),
+  ),
+  array
+  (
+    "for" => "red4", "label" => "4th Note Color", "choices" => array
+    (
+      array("value" => "blue", "text" => "Red", "selected" => true),
+      array("value" => "red", "text" => "Blue", "selected" => false),
+    ),
+  ),
+  array
+  (
+    "for" => "speed", "label" => "Speed Mod", "choices" => array
+    (
+      array("value" => 1, "text" => 1, "selected" => false),
+      array("value" => 2, "text" => 2, "selected" => true),
+      array("value" => 3, "text" => 3, "selected" => false),
+      array("value" => 4, "text" => 4, "selected" => false),
+      array("value" => 6, "text" => 6, "selected" => false),
+      array("value" => 8, "text" => 8, "selected" => false),
+    ),
+  ),
+  array
+  (
+    "for" => "mpcol", "label" => "Measures per column", "choices" => array
+    (
+      array("value" => 4, "text" => 4, "selected" => false),
+      array("value" => 6, "text" => 6, "selected" => true),
+      array("value" => 8, "text" => 8, "selected" => false),
+      array("value" => 12, "text" => 12, "selected" => false),
+      array("value" => 16, "text" => 16, "selected" => false),
+    ),
+  ),
+  array
+  (
+    "for" => "scale", "label" => "Scale Factor", "choices" => array
+    (
+      array("value" => 0.5, "text" => 0.5, "selected" => false),
+      array("value" => 0.75, "text" => 0.75, "selected" => false),
+      array("value" => 1, "text" => 1, "selected" => true),
+      array("value" => 1.25, "text" => 1.25, "selected" => false),
+      array("value" => 1.5, "text" => 1.5, "selected" => false),
+      array("value" => 1.75, "text" => 1.75, "selected" => false),
+      array("value" => 2, "text" => 2, "selected" => false),
+    ),
+  ),
+); ?>
+
+<section id="options" class="grid_5 omega">
+<?php foreach ($options as $option): ?>
+<div class="grid_3 alpha"><label for="<?php echo $option["for"]; ?>"><?php echo $option['label']; ?></label></div>
+<div class="grid_2 omega pushDown"><select id="<?php echo $option["for"]; ?>" name="<?php echo $option["for"]; ?>">
+<?php foreach ($option['choices'] as $choice): ?>
+<option <?php if ($choice['selected']) { ?>selected="selected" <?php } ?>value="<?php echo $choice['value']; ?>"><?php echo $choice['text']; ?></option>
+<?php endforeach; ?>
+</select></div>
+<div class="reset"></div>
+<?php endforeach; ?>
 </section>
+
+</div>
+<div class="clear"></div>
 <p><button id="submit" name="submit" type="submit" value="submit">Submit!</button></p>
 
 </fieldset>
