caac92883dfea4ec45ddfe4ee876aa3e55aa097f
Jason Felds <jafelds@gmail.com>
2010-07-29 16:05:15 -0400

    No longer using these files.

--- application/views/global/memb_out.php
+++
@@ -1,5 +0,0 @@
-<li><?php echo anchor("/register", "Register"); ?></li>
-<li><?php echo anchor("/confirm", "Confirm Account"); ?></li>
-<li><?php echo anchor("/login", "Log In"); ?></li>
-<li><?php echo anchor("/help", "Account Help"); ?></li>
-<li><?php echo anchor("/reset", "Reset Password"); ?></li>
