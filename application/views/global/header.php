bb41cb0168e2c5fc1a27b415aadd887fe8826dd3
Jason Felds <jafelds@gmail.com>
2010-07-29 16:09:32 -0400

    Reuse separate nav file.

--- application/views/global/header.php
+++ application/views/global/header.php
@@ -89,41 +89,7 @@ foreach ($baseScripts as $script): ?>
   </div>
 </header>
 <div class="clear"></div>
-<nav id="main_nav" class="grid_12">
-<?php $uid = $this->session->userdata('id');
-
-# $this->load->view('global/mess_' . ($uid === false ? 'out' : 'in')); ?>
-<ul class="sf-menu">
-<li><?php echo anchor("/", "Home"); ?></li>
-<?php if (!$uid and $this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
-<li><?php echo anchor("#", "Account Stuff"); ?><ul>
-<li><?php echo anchor("/confirm", "Confirm Account"); ?></li>
-<li><?php echo anchor("/help", "Account Help"); ?></li>
-<li><?php echo anchor("/reset", "Reset Password"); ?></li>
-</ul></li>
-<?php endif; ?>
-<li><?php echo anchor("#", "Edits"); ?><ul>
-<li><?php echo anchor("/songs", "List by Song"); ?></li>
-<li><?php echo anchor("/users", "List by Author"); ?></li>
-<li><?php echo anchor("/official", "List Official Edits"); ?></li>
-<li><?php echo anchor("/create", "Edit Creator"); ?></li>
-<?php if ($uid): ?>
-<li><?php echo anchor("/remove", "Remove Edits"); ?></li>
-<?php endif; ?>
-</ul></li>
-<?php if ($this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
-<li><?php echo anchor("#", "Stepcharts"); ?><ul>
-<li><?php echo anchor("/chart", "View Edit Stepcharts"); ?></li>
-<li><?php echo anchor("/chart/songs", "View Official Stepcharts"); ?></li>
-</ul></li>
-<?php endif; ?>
-<li><?php echo anchor("/usb", "USB Guide"); ?></li>
-<li><?php echo anchor("/contact", "Contact"); ?></li>
-<li><?php echo anchor("/thanks", "Credits"); ?></li>
-<li><a href="http://www.pumpproedits.com/blog">Blog</a></li>
-</ul>
-</nav>
-
+<?php $this->load->view('global/nav_normal'); ?>
 <article class="grid_12">
 <h2><?php if (!(isset($h2))) { $h2 = "Welcome!"; } echo $h2; ?></h2>
 <div id="bd">
--- application/views/global/nav_normal.php
+++ application/views/global/nav_normal.php
@@ -1,64 +1,35 @@
-<nav id="main_nav">
 
-
+<nav id="main_nav" class="grid_12">
 <?php $uid = $this->session->userdata('id');
 
 # $this->load->view('global/mess_' . ($uid === false ? 'out' : 'in')); ?>
 <ul class="sf-menu">
 <li><?php echo anchor("/", "Home"); ?></li>
-<li><?php echo anchor("#", "Account Stuff"); ?></li>
+<?php if (!$uid and $this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
+<li><?php echo anchor("#", "Account Stuff"); ?><ul>
+<li><?php echo anchor("/confirm", "Confirm Account"); ?></li>
+<li><?php echo anchor("/help", "Account Help"); ?></li>
+<li><?php echo anchor("/reset", "Reset Password"); ?></li>
+</ul></li>
+<?php endif; ?>
 <li><?php echo anchor("#", "Edits"); ?><ul>
 <li><?php echo anchor("/songs", "List by Song"); ?></li>
 <li><?php echo anchor("/users", "List by Author"); ?></li>
 <li><?php echo anchor("/official", "List Official Edits"); ?></li>
 <li><?php echo anchor("/create", "Edit Creator"); ?></li>
+<?php if ($uid): ?>
+<li><?php echo anchor("/remove", "Remove Edits"); ?></li>
+<?php endif; ?>
 </ul></li>
+<?php if ($this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
 <li><?php echo anchor("#", "Stepcharts"); ?><ul>
 <li><?php echo anchor("/chart", "View Edit Stepcharts"); ?></li>
 <li><?php echo anchor("/chart/songs", "View Official Stepcharts"); ?></li>
 </ul></li>
-<li><?php echo anchor("/usb", "USB Usage"); ?></li>
+<?php endif; ?>
+<li><?php echo anchor("/usb", "USB Guide"); ?></li>
 <li><?php echo anchor("/contact", "Contact"); ?></li>
 <li><?php echo anchor("/thanks", "Credits"); ?></li>
 <li><a href="http://www.pumpproedits.com/blog">Blog</a></li>
-
 </ul>
 </nav>
-
-<!--
-<li>
-<h4>Members</h4>
-<ul>
-<?php $this->load->view('global/memb_' . ($uid === false ? 'out' : 'in')); ?>
-</ul>
-</li>
-<li>
-<h4>Edits</h4>
-<ul>
-<?php if ($this->session->userdata('browser') === "Internet Explorer"): ?>
-<li><?php echo anchor("/base", "Base Edit Files"); ?></li>
-<li><?php echo anchor("/stats", "Edit Stat Getter"); ?></li>
-<?php else: ?>
-<li><?php echo anchor("/create", "Edit Creator"); ?></li>
-<li><?php echo anchor("/chart", "Edit Charter"); ?></li>
-<?php endif; ?>
-<li><?php echo anchor("/songs", "Edit List by Song"); ?></li>
-<li><?php echo anchor("/users", "Edit List by User"); ?></li>
-<li><?php echo anchor("/official", "Official Chart Edits"); ?></li>
-</ul>
-</li>
-<li>
-<h4>Everyone</h4>
-<ul>
-<li><a href="/blog">Pump Pro Edits Blog</a></li>
-<?php if ($this->session->userdata('browser') !== "Internet Explorer"): ?>
-<li><?php echo anchor("/chart/songs", "Official Stepcharts"); ?></li>
-<?php endif; ?>
-<li><?php echo anchor("/usb", "USB Usage"); ?></li>
-<li><?php echo anchor("/contact", "Contact"); ?></li>
-<li><?php echo anchor("/thanks", "Credits/Thanks"); ?></li>
-</ul>
-</li>
-</ul>
-</nav>
--->
