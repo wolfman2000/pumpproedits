4ce026025bd2f18ca884924f61ab5b3b3f428ef9
Jason Felds <jafelds@gmail.com>
2010-07-29 16:03:18 -0400

    Remove old stuff.

--- application/views/global/header.php
+++ application/views/global/header.php
@@ -121,35 +121,9 @@ foreach ($baseScripts as $script): ?>
 <li><?php echo anchor("/contact", "Contact"); ?></li>
 <li><?php echo anchor("/thanks", "Credits"); ?></li>
 <li><a href="http://www.pumpproedits.com/blog">Blog</a></li>
-
 </ul>
 </nav>
 
-<!--
-<li>
-<h4>Members</h4>
-<ul>
-<?php $this->load->view('global/memb_' . ($uid === false ? 'out' : 'in')); ?>
-</ul>
-</li>
-<li>
-<h4>Edits</h4>
-<ul>
-<?php if ($this->session->userdata('browser') === "Internet Explorer"): ?>
-<li><?php echo anchor("/base", "Base Edit Files"); ?></li>
-<li><?php echo anchor("/stats", "Edit Stat Getter"); ?></li>
-<?php else: ?>
-<li><?php echo anchor("/create", "Edit Creator"); ?></li>
-<li><?php echo anchor("/chart", "Edit Charter"); ?></li>
-<?php endif; ?>
-<li><?php echo anchor("/songs", "Edit List by Song"); ?></li>
-<li><?php echo anchor("/users", "Edit List by User"); ?></li>
-<li><?php echo anchor("/official", "Official Chart Edits"); ?></li>
-</ul>
-</li>
-</ul>
-</nav>
--->
 <article class="grid_12">
 <h2><?php if (!(isset($h2))) { $h2 = "Welcome!"; } echo $h2; ?></h2>
 <div id="bd">
