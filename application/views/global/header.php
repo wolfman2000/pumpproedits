b289f9f15ddae4a313e597d0cc5ab379f37d4ad7
Jason Felds <jafelds@gmail.com>
2010-07-29 15:57:29 -0400

    Add other account options.

--- application/views/global/header.php
+++ application/views/global/header.php
@@ -95,14 +95,21 @@ foreach ($baseScripts as $script): ?>
 # $this->load->view('global/mess_' . ($uid === false ? 'out' : 'in')); ?>
 <ul class="sf-menu">
 <li><?php echo anchor("/", "Home"); ?></li>
-<?php if ($this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
-<li><?php echo anchor("#", "Account Stuff"); ?></li>
+<?php if (!$uid and $this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
+<li><?php echo anchor("#", "Account Stuff"); ?><ul>
+<li><?php echo anchor("/confirm", "Confirm Account"); ?></li>
+<li><?php echo anchor("/help", "Account Help"); ?></li>
+<li><?php echo anchor("/reset", "Reset Password"); ?></li>
+</ul></li>
 <?php endif; ?>
 <li><?php echo anchor("#", "Edits"); ?><ul>
 <li><?php echo anchor("/songs", "List by Song"); ?></li>
 <li><?php echo anchor("/users", "List by Author"); ?></li>
 <li><?php echo anchor("/official", "List Official Edits"); ?></li>
 <li><?php echo anchor("/create", "Edit Creator"); ?></li>
+<?php if ($uid): ?>
+<li><?php echo anchor("/remove", "Remove Edits"); ?></li>
+<?php endif; ?>
 </ul></li>
 <?php if ($this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
 <li><?php echo anchor("#", "Stepcharts"); ?><ul>
