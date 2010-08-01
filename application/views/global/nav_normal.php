0b04fa15ca46b3598abd69ec34a586fa4e87943e
Jason Felds <jafelds@gmail.com>
2010-08-01 12:12:24 -0400

    Adjust nav. Edit Creator stands alone.

--- application/views/global/nav_normal.php
+++ application/views/global/nav_normal.php
@@ -12,17 +12,17 @@
 <li><?php echo anchor("/reset", "Reset Password"); ?></li>
 </ul></li>
 <?php endif; ?>
-<li><?php echo anchor("#", "Edits"); ?><ul>
+<li><?php echo anchor("#", "Browse Edits"); ?><ul>
 <li><?php echo anchor("/songs", "List by Song"); ?></li>
 <li><?php echo anchor("/users", "List by Author"); ?></li>
 <li><?php echo anchor("/official", "List Official Edits"); ?></li>
-<li><?php echo anchor("/create", "Edit Creator"); ?></li>
 <?php if ($uid): ?>
 <li><?php echo anchor("/remove", "Remove Edits"); ?></li>
 <?php endif; ?>
 </ul></li>
 <?php if ($this->agent->is_browser() and $this->session->userdata('browser') !== "Internet Explorer"): ?>
-<li><?php echo anchor("#", "Stepcharts"); ?><ul>
+<li><?php echo anchor("/create", "Edit Creator"); ?></li>
+<li><?php echo anchor("#", "View Stepcharts"); ?><ul>
 <li><?php echo anchor("/chart", "View Edit Stepcharts"); ?></li>
 <li><?php echo anchor("/chart/songs", "View Official Stepcharts"); ?></li>
 </ul></li>
