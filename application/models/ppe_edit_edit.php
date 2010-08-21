af2b2608d59a0238c16339a6fc0c31b090fab873
Jason Felds <jafelds@gmail.com>
2010-08-20 23:11:44 -0400

    Add radars. Retrieve edit/player ID combo. Next up will be inserting the measure data.

--- application/models/ppe_edit_edit.php
+++ application/models/ppe_edit_edit.php
@@ -35,7 +35,15 @@ class Ppe_edit_edit extends Model
     }
     foreach ($players as $player)
     {
+    	$pid = $this->db->select('MAX(id) AS pid')->get('ppe_edit_player')
+    		->row()->pid + 1;
        $data = array(
+       	   'id' => $pid,
+       	   'air' => $row['air'][$player],
+       	   'stream' => $row['stream'][$player],
+       	   'voltage' => $row['voltage'][$player],
+       	   'freeze' => $row['freeze'][$player],
+       	   'chaos' => $row['chaos'][$player],
         'steps' => $row['steps'][$player],
         'jumps' => $row['jumps'][$player],
         'holds' => $row['holds'][$player],
@@ -48,6 +56,9 @@ class Ppe_edit_edit extends Model
         'edit_id' => $id,
       );
       $this->db->insert('ppe_edit_player', $data);
+      
+      // insert note data here.
+      
     }
     return $id; // Return the edit ID.
   }
@@ -69,7 +80,13 @@ class Ppe_edit_edit extends Model
     }
     foreach ($players as $player)
     {
+    	
       $data = array(
+      	  'air' => $row['air'][$player],
+       	   'stream' => $row['stream'][$player],
+       	   'voltage' => $row['voltage'][$player],
+       	   'freeze' => $row['freeze'][$player],
+       	   'chaos' => $row['chaos'][$player],
         'steps' => $row['steps'][$player],
         'jumps' => $row['jumps'][$player],
         'holds' => $row['holds'][$player],
@@ -80,7 +97,13 @@ class Ppe_edit_edit extends Model
         'fakes' => $row['fakes'][$player],
       );
       $where = array('edit_id' => $id, 'player' => $player + 1);
+      
       $this->db->update('ppe_edit_player', $data, $where);
+      $pid = $this->db->select('id')->get('ppe_edit_player')
+    		where($where)->row()->id;
+      
+      // remove what's there, and reupload.
+      
     }
   }
   
