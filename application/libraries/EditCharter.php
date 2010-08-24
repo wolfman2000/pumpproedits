2a5b7065d2f3b2e6a4b37152d4b79dd8edb0106b
Jason Felds <jafelds@gmail.com>
2010-08-23 23:37:10 -0400

    Fix displaying of all notes on Routine mode. Player fixes will be later: bed is soon.

--- application/libraries/EditCharter.php
+++ application/libraries/EditCharter.php
@@ -500,21 +500,24 @@ class EditCharter
       case "M": # Mine. Don't step on these!
       {
         $holds[$pcounter]['on'] = false;
-        $opt = array('href' => 'mine', 'class' => $arow[$pcounter]['c']);
+        $tmp = ($style === "pump-routine" ? "P" . $ucounter: "");
+        $opt = array('href' => $tmp . 'mine', 'class' => $arow[$pcounter]['c']);
         $nt->appendChild($this->xml->importNode($sm->genUse($nx, $ny, $opt)));
         break;
       }
       case "L": # Lift note. Can be placed in chart. No image yet.
       {
         $holds[$pcounter]['on'] = false;
-        $opt = array('href' => 'lift', 'class' => $arow[$pcounter]['c']);
+        $tmp = ($style === "pump-routine" ? "P" . $ucounter: "");
+        $opt = array('href' => $tmp . 'lift', 'class' => $arow[$pcounter]['c']);
         $nt->appendChild($this->xml->importNode($sm->genUse($nx, $ny, $opt)));
         break;
       }
       case "F": # Fake note. Officially in Pro 2.
       {
         $holds[$pcounter]['on'] = false;
-        $opt = array('href' => 'fake', 'class' => $arow[$pcounter]['c']);
+        $tmp = ($style === "pump-routine" ? "P" . $ucounter: "");
+        $opt = array('href' => $tmp . 'fake', 'class' => $arow[$pcounter]['c']);
         $nt->appendChild($this->xml->importNode($sm->genUse($nx, $ny, $opt)));
         break;
       }
