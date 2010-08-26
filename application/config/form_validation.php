f650dc3e7063d8e056f702db3c9c08e4523532e5
Jason Felds <jafelds@gmail.com>
2010-08-26 17:25:20 -0400

    Main refactoring complete.

--- application/config/form_validation.php
+++ application/config/form_validation.php
@@ -29,10 +29,22 @@ $chartCommon = array(
 	),
 );
 $chartEdit = $chartCommon;
+$chartSong = $chartCommon;
+
 $chartEdit[] = array(
 	'field' => 'edits',
 	'label' => 'Edit',
 	'rules' => 'callback__edit_exists'
+);
+$chartSong[] = array(
+	'field' => 'songs',
+	'label' => 'Edit',
+	'rules' => 'is_natural_no_zero'
+);
+$chartSong[] = array(
+	'field' => 'diff',
+	'label' => 'Difficulty',
+	'rules' => 'callback__diff_exists'
 );
 
 $config = array(
@@ -133,73 +145,6 @@ $config = array(
 			'rules' => 'required|min_length[20]'
 		),
 	),
-	'chart/editProcess' => array(
-		array(
-			'field' => 'edits',
-			'label' => 'Edit',
-			'rules' => 'callback__edit_exists'
-		),
-		array(
-			'field' => 'kind',
-			'label' => 'Noteskin',
-			'rules' => 'callback__noteskin_exists'
-		),
-		array(
-			'field' => 'red4',
-			'label' => '4th Note Color',
-			'rules' => 'callback__red_exists'
-		),
-		array(
-			'field' => 'speed',
-			'label' => 'Speed Mod',
-			'rules' => 'callback__speed_valid'
-		),
-		array(
-			'field' => 'mpcol',
-			'label' => 'Measures per Column',
-			'rules' => 'callback__mpc_valid'
-		),
-		array(
-			'field' => 'scale',
-			'label' => 'Scale',
-			'rules' => 'callback__scale_valid'
-		),
-	),
-	'chart/songProcess' => array(
-		array(
-			'field' => 'songs',
-			'label' => 'Edit',
-			'rules' => 'is_natural_no_zero'
-		),
-		array(
-			'field' => 'diff',
-			'label' => 'Difficulty',
-			'rules' => 'callback__diff_exists'
-		),
-		array(
-			'field' => 'kind',
-			'label' => 'Noteskin',
-			'rules' => 'callback__noteskin_exists'
-		),
-		array(
-			'field' => 'red4',
-			'label' => '4th Note Color',
-			'rules' => 'callback__red_exists'
-		),
-		array(
-			'field' => 'speed',
-			'label' => 'Speed Mod',
-			'rules' => 'callback__speed_valid'
-		),
-		array(
-			'field' => 'mpcol',
-			'label' => 'Measures per Column',
-			'rules' => 'callback__mpc_valid'
-		),
-		array(
-			'field' => 'scale',
-			'label' => 'Scale',
-			'rules' => 'callback__scale_valid'
-		),
-	)
+	'chart/editProcess' => $chartEdit,
+	'chart/songProcess' => $chartSong,
 );
