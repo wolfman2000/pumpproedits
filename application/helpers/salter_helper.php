e14e7a1bf3411fdbb047b07315723329b9524bd6
Jason Felds <jafelds@gmail.com>
2010-09-07 01:03:25 -0400

    The helpers still in use now.

--- application/helpers/salter_helper.php
+++ application/helpers/salter_helper.php
@@ -1,4 +1,12 @@
 <?php
+/*
+PHP file used for Pump Pro Edits
+
+@package pumpproedits
+@author wolfman2000
+@license GNU Affero GPL v3 or later
+*/
+
 function genSalt($length = 5)
 {
 	if ($length < 1) { return ''; }
