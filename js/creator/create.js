9a3f9d14126e38bebdac89b81aa6f759f82e3711
Jason Felds <jafelds@gmail.com>
2010-09-17 16:55:25 -0400

    Move admin functionality to dedicated file.

--- js/creator/create.js
+++ js/creator/create.js
@@ -120,43 +120,6 @@ $(document).ready(function()
     }
   });
   
-  // The admin has chosen a song, and needs to choose a difficulty.
-  $("#loadSong").change(function(){
-    songID = $("#loadSong").val();
-    if (songID.length > 0)
-    {
-      $("#intro").text("Setting up styles...")
-      _disable("#loadDifficulty");
-      $.getJSON(baseURL + "/songDifficulties/" + songID, function(data, status)
-      {
-        var diff = $("#loadDifficulty").val();
-        if (data.isRoutine > 0) { $("#loadDifficulty > option:last-child").show(); }
-        else                    { $("#loadDifficulty > option:last-child").hide();
-          if (diff == "rt")
-          {
-            $("#loadDifficulty").val("");
-            _disable("#song_yes");
-          }
-        }
-        _enable("#loadDifficulty");
-      });
-    }
-    else
-    {
-      _disable("#loadDifficulty");
-      _enable("#song_yes");
-    }
-  });
-  
-  // The admin has chosen a song and style, and thus can load the chart.
-  $("#loadDifficulty").change(function(){
-    if ($("#loadDifficulty").val().length) { _enable("#song_yes"); }
-    else                                   { _disable("#song_yes"); }
-  });
-  
-  // The admin is ready to load the chart (if it exists)
-  $("#song_yes").click(function(){ songMode(); });
-  
   // The edit contents have to be placed in here due to AJAX requirements.
   $("#fCont").keyup(function(){
     if ($("#fCont").val().length)
