de964dd24575d3e83e87e8eb1073cbdac0bd8690
Jason Felds <jafelds@gmail.com>
2010-09-17 01:09:14 -0400

    The edits are taken care of. More refactoring tomorrow...unsure when.

--- application/views/edits/users.php
+++ application/views/edits/users.php
@@ -5,11 +5,8 @@ PHP file used for Pump Pro Edits
 @package pumpproedits
 @author wolfman2000
 @license GNU Affero GPL v3 or later
-*/
-$this->load->view('global/header',
-  array('css' => 'css/edit_count.css', 'h2' => 'Edits by User', 'title' => 'Edit List by User')); ?>
+*/ ?>
 <p>Below are all of the users that have edits available.</p>
 
 <?php $this->load->view('edits/counter',
   array('query' => $query, 'what' => 'user'));
-$this->load->view('global/footer');
