25abea51f7b8576114cc85153af696ec91fd5705
Jason Felds <jafelds@gmail.com>
2010-09-17 15:08:54 -0400

    Start the move to avoid consts. Only load the functions needed.

---
+++ js/creator/auth_basic.js
@@ -0,0 +1,22 @@
+/*
+ * This file handles the functions used for the users
+ * that are authenticated to make their own edits
+ *
+ * Should more power be given to the user, the functions in
+ * this file may end up being overridden.
+ */
+/*
+JS file used for Pump Pro Edits
+
+@package pumpproedits
+@author wolfman2000
+@license GNU Affero GPL v3 or later
+*/
+
+function loadButtons()
+{
+	$("li.edit").hide();
+	$("li.loadWeb").show();
+	$("li[class^=load]:not(.loadWeb)").hide();
+	$("#intro").text("Select your option.");
+}
---
+++ js/creator/auth_none.js
@@ -0,0 +1,20 @@
+/*
+ * This file handles the functions used for the users
+ * that are not authenticated at all.
+ *
+ * Should any users be authenticated, the functions in
+ * this file may end up being overridden.
+ */
+/*
+JS file used for Pump Pro Edits
+
+@package pumpproedits
+@author wolfman2000
+@license GNU Affero GPL v3 or later
+*/
+
+function loadButtons()
+{
+	$("li.edit").hide();
+	loadHardDrive();
+}
