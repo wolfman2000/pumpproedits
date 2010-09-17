60c06dee6ee38725ad7e57c3de25fff7263e4778
Jason Felds <jafelds@gmail.com>
2010-09-16 21:48:32 -0400

    Convert the Contact pages.

--- application/views/contact/main.php
+++ application/views/contact/main.php
@@ -5,9 +5,7 @@ PHP file used for Pump Pro Edits
 @package pumpproedits
 @author wolfman2000
 @license GNU Affero GPL v3 or later
-*/
-$this->load->view('global/header',
-  array('css' => 'css/contact.css', 'h2' => 'Contact the Webmaster', 'title' => 'Contact the Webmaster')); ?>
+*/ ?>
 <p>If you need to reach the web master for any reason, you have
 two easy ways of doing so. Those familiar with sending email
 through client programs such as Apple's Mail or Mozilla Thunderbird
@@ -15,4 +13,3 @@ can <a href="mailto:jafelds@gmail.com">email me directly.</a> A
 form alternative is also provided below. Use whichever is convenient
 for you.</p>
 <?php $this->load->view('contact/form');
-$this->load->view('global/footer');
