521dc67f013e7e7f11ea67d42ceead0c35f5dc10
Jason Felds <jafelds@gmail.com>
2010-09-16 22:13:30 -0400

    The reset password pages are fixed now.

--- application/controllers/reset.php
+++ application/controllers/reset.php
@@ -7,62 +7,74 @@ PHP File for Pump Pro Edits
 @license GNU Affero GPL v3 or later
 */
 
-class Reset extends Controller
+class Reset extends Wolf_Controller
 {
-  function __construct()
-  {
-    parent::Controller();
-    $this->load->helper('form');
-    $this->load->library('form_validation');
-    $this->form_validation->set_error_delimiters('<p class="error_list">', '</p>');
-
-  }
-  
-  function index()
-  {
-    $this->load->view('reset/main');
-  }
-  
-  function check()
-  {
-    if ($this->form_validation->run() === FALSE)
-    {
-      $this->load->view('reset/missing');
-      return;
-    }
-    $oreg = $this->input->post('confirm');
-    $this->load->model('ppe_user_condiment');
-    $this->load->model('ppe_user_role');
-    $this->load->model('ppe_user_user');
-    $id = $this->ppe_user_condiment->getIDByOregano($oreg);
-
-    if (!$id)
-    {
-      $this->output->set_status_header(409);
-      $this->load->view('reset/mismatch');
-      //array_push($this->data, "Make sure you put in the confirmation code correctly.");
-    }
-    // rare to be banned while getting helped, but here it is...
-    elseif ($this->ppe_user_role->getIsUserBanned($id))
-    {
-      $this->output->set_status_header(409);
-      $this->load->view('reset/banned');
-    }
-    /* Disable this for now: something isn't right.    
-    elseif ($this->ppe_user_user->getConfirmedByID($id)) // If confirmed, don't allow reseting.
-    {
-      $this->output->set_status_header(409);
-      $this->load->view('reset/noneed');
-    }
-    */
-    else // We're good!
-    {
-      $this->ppe_user_user->confirmUser($id);
-      $this->ppe_user_condiment->setPassword($id, $this->input->post('password'));
-      $this->session->set_userdata('id', $id);
-      $this->session->set_userdata('roles', $this->ppe_user_role->getRolesByID($id));
-      $this->session->set_userdata('username', $this->ppe_user_user->getUserByID($id));
-      $this->load->view('reset/success');
-    }
-  }
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+		$this->form_validation->set_error_delimiters('<p class="error_list">', '</p>');
+		$this->_setCSS('css/reset.css');
+	}
+	
+	function index()
+	{
+		$this->_setHeader('Reset your Password');
+		$this->_setTitle('Reset your Password');
+		$this->_loadPage('reset/main');
+	}
+	
+	function check()
+	{
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->_setHeader('Password Resetting Unsuccessful');
+			$this->_setTitle('Password Resetting Unsuccessful');
+			$this->_loadPage('reset/missing');
+			return;
+		}
+		$oreg = $this->input->post('confirm');
+		$this->load->model('ppe_user_condiment');
+		$this->load->model('ppe_user_role');
+		$this->load->model('ppe_user_user');
+		$id = $this->ppe_user_condiment->getIDByOregano($oreg);
+		
+		if (!$id)
+		{
+			$this->output->set_status_header(409);
+			$this->_setTitle('Password Resetting Unsuccessful');
+			$this->_setHeader('Password Resetting Unsuccessful');
+			$this->_loadPage('reset/mismatch');
+			//array_push($this->data, "Make sure you put in the confirmation code correctly.");
+		}
+		// rare to be banned while getting helped, but here it is...
+		elseif ($this->ppe_user_role->getIsUserBanned($id))
+		{
+			$this->_setHeader('Password Reset Unsuccessful');
+			$this->_setTitle('Password Reset Unsuccessful');
+			$this->output->set_status_header(409);
+			$this->_loadPage('reset/banned');
+		}
+		/* Disable this for now: something isn't right.    
+		elseif ($this->ppe_user_user->getConfirmedByID($id)) // If confirmed, don't allow reseting.
+		{
+			$this->output->set_status_header(409);
+			$this->_setHeader('No Need to Reset Password');
+			$this->_setTitle('No Need to Reset Password');
+			$this->_loadPage('reset/noneed');
+		}
+		*/
+		else // We're good!
+		{
+			$this->ppe_user_user->confirmUser($id);
+			$this->ppe_user_condiment->setPassword($id, $this->input->post('password'));
+			$this->session->set_userdata('id', $id);
+			$this->session->set_userdata('roles', $this->ppe_user_role->getRolesByID($id));
+			$this->session->set_userdata('username', $this->ppe_user_user->getUserByID($id));
+			$this->_setHeader('Password Reset Successful');
+			$this->_setTitle('Password Reset Successful');
+			$this->_loadPage('reset/success');
+		}
+	}
 }
--- application/views/reset/banned.php
+++ application/views/reset/banned.php
@@ -5,10 +5,7 @@ PHP file used for Pump Pro Edits
 @package pumpproedits
 @author wolfman2000
 @license GNU Affero GPL v3 or later
-*/
-$this->load->view('global/header',
-  array('css' => 'css/reset.css', 'h2' => 'Password Reset Unsuccessful', 'title' => 'Password Reset Unsuccessful')); ?>
+*/ ?>
 <p>The resetting was unsuccessful. You are apparently banned from using
 the member functions of the website. If you feel this is in error, contact
 the web author.</p>
-<?php $this->load->view('global/footer');
