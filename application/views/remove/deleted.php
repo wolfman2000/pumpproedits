af677e88ca33e227dab23fe1c6a0b8b333ec4024
Jason Felds <jafelds@gmail.com>
2010-09-16 22:34:08 -0400

    Convert removing edits. Fix bad typo.

--- application/views/remove/deleted.php
+++ application/views/remove/deleted.php
@@ -5,10 +5,7 @@ PHP file used for Pump Pro Edits
 @package pumpproedits
 @author wolfman2000
 @license GNU Affero GPL v3 or later
-*/
-$this->load->view('global/header',
-  array('css' => 'css/remove.css', 'h2' => 'Edits Removed', 'title' => 'Edits Removed')); ?>
+*/ ?>
 <p>The selected edits have been removed from the system.
 If you wish to remove more, use the form below.</p>
 <?php $this->load->view('remove/form');
-$this->load->view('global/footer');
--- application/views/remove/main.php
+++ application/views/remove/main.php
@@ -5,11 +5,8 @@ PHP file used for Pump Pro Edits
 @package pumpproedits
 @author wolfman2000
 @license GNU Affero GPL v3 or later
-*/
-$this->load->view('global/header',
-  array('css' => 'css/remove.css', 'h2' => 'Remove your Edits', 'title' => 'Remove your Edits')); ?>
+*/ ?>
 <p>If you feel it's time to remove your edits from the system,
 use the form below for that. Note that edit recovery may not be possible
 if you change your mind, so think about it carefully.</p>
 <?php $this->load->view('remove/form');
-$this->load->view('global/footer');
