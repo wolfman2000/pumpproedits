93f5e3c31c4fa3056b6b97ffb0cbd10aa71b15d5
Jason Felds <jafelds@gmail.com>
2010-09-16 21:26:55 -0400

    Get the new entry page set up.

--- application/controllers/ppehome.php
+++ application/controllers/ppehome.php
@@ -17,9 +17,9 @@ class Ppehome extends Wolf_Controller
 	function index()
 	{
 		$this->load->model('ppe_edit_edit');
-		$data['query'] = $this->ppe_edit_edit->getEditsEntry()->result();
-		$data['showuser'] = 1;
-		$data['showsong'] = 1;
-		$this->load->view('ppehome/main', $data);
+		$this->data['query'] = $this->ppe_edit_edit->getEditsEntry()->result();
+		$this->data['showuser'] = 1;
+		$this->data['showsong'] = 1;
+		$this->_loadPage('ppehome/main');
 	}
 }
