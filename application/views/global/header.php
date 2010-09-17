d895d9212fcbba89f7490df0891f0ef63f4a3f48
Jason Felds <jafelds@gmail.com>
2010-09-16 21:31:13 -0400

    Simplify if:

--- application/views/global/header.php
+++ application/views/global/header.php
@@ -57,7 +57,7 @@ const songID = <?php echo $const_song; ?>;
   </a></h1>
   <div class="grid_5 alpha" id="userbar">
     <ul>
-      <?php if ($browser === "ie" and browser_detection('ie_version') !== "ie9x"): ?>
+      <?php if (!$modern): ?>
       <li><a href="http://www.firefox.com">Firefox</a></li>
       <li><a href="http://chrome.google.com">Chrome</a></li>
       <li><a href="http://www.apple.com/safari">Safari</a></li>
