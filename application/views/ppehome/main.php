83058c6da8674cbb28481dc85514dd57d112df87
Jason Felds <jafelds@gmail.com>
2010-09-20 12:51:03 -0400

    Provide a different message for IE9 users.

--- application/views/ppehome/main.php
+++ application/views/ppehome/main.php
@@ -9,10 +9,14 @@ PHP file used for Pump Pro Edits
 <p>Welcome to the Pump Pro Edit database. Inside here, you will find
 many edits that dance players such as yourself have created, along
 with official charts that Andamiro and Fun In Motion staff made themselves.</p>
-<?php if ($browser == "ie"): ?>
+<?php if (!$modern): ?>
 <p>To actually view the charts in a graphical format or contribute
 edit content to the website, you will have to use a different web
-browser. The recommended browsers are listed at the top of this page.
+browser. The recommended browsers are listed at the top of this page.</p>
+<?php elseif ($browser == "ie"): ?>
+<p>In terms of special features, you are able to view the charts in the
+graphical SVG format. In order to properly use the Edit Creator, however,
+it is recommended to use a browser that is listed at the top of this page.</p>
 <?php else: ?>
 <p>Want to make an edit yourself? There is a new
 <?php echo anchor("/create", "Edit Creator"); ?> tool
