e730c82ea7cf591ccd9421433e35bef6f29f6f0b
Jason Felds <jafelds@gmail.com>
2010-10-10 12:50:00 -0400

    New password plan.

--- application/config/database.php
+++ application/config/database.php
@@ -1,4 +1,5 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+require_once('db_password.php');
 /*
 | -------------------------------------------------------------------
 | DATABASE CONNECTIVITY SETTINGS
