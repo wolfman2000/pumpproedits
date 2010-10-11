de3ba0c34dfaa5d08ffe4049641a7525b69ac750
Jason Felds <jafelds@gmail.com>
2010-10-10 23:00:20 -0400

    Bring it in line. Supply temp code.

--- application/controllers/chart.php
+++ application/controllers/chart.php
@@ -11,15 +11,15 @@ class Chart extends Controller
 		$this->load->model('itg_song_song');
 		$this->load->model('itg_user_user');
 		$this->difficulties = array('sb', 'se', 'sm', 'sh', 'sx', 'de', 'dm', 'dh', 'dx');
-  	}
-  
-  // confirm the song and difficulty exist.
-  function _diff_exists($str)
-  {
-    if (in_array($str, $this->difficulties)) return true;
-    $this->form_validation->set_message('_diff_exists', 'A valid difficulty must be chosen.');
-    return false;
-  }
+	}
+	
+	// confirm the song and difficulty exist.
+	function _diff_exists($str)
+	{
+		if (in_array($str, $this->difficulties)) return true;
+		$this->form_validation->set_message('_diff_exists', 'A valid difficulty must be chosen.');
+		return false;
+	}
   
   // confirm the edit exists.
   function _edit_exists($str)
@@ -27,31 +27,53 @@ class Chart extends Controller
     if ($this->itg_edit_edit->checkExistance($str)) return true;
     $this->form_validation->set_message('_edit_exists', "The edit chosen $str doesn't have a corresponding file.");
     return false;
-  }
-  
-  // confirm the speed mod is valid.
-  function _speed_valid($str)
-  {
-    if (in_array($str, array(1, 2, 3, 4, 6, 8))) return true;
-    $this->form_validation->set_message('_speed_valid', 'A valid speed mod must be chosen.');
-    return false;
-  }
-  
-  // confirm the number of measures in each column is valid.
-  function _mpc_valid($str)
-  {
-    if (in_array($str, array(4, 6, 8, 12, 16))) return true;
-    $this->form_validation->set_message('_mpc_valid', 'An unknown number of measures per column was chosen.');
-    return false;
-  }
-  
-  // confirm the scale factor is valid.
-  function _scale_valid($str)
-  {
-    if (in_array($str, array(0.5, 0.75, 1, 1.25, 1.5, 1.75, 2))) return true;
-    $this->form_validation->set_message('_edit_exists', 'The scale chosen was not a valid scale.');
-    return false;
-  }
+	}
+	
+	// confirm the note color exists.
+	function _notecolor_exists($str)
+	{
+		if (in_array($str, $this->ppe_note_style->getNoteStyles(1))) return true;
+		$this->form_validation->set_message('_notecolor_exists', "Please choose a valid note style.");
+		return false;
+	}
+	
+	// confirm the note skin exists.
+	function _noteskin_exists($str)
+	{
+		if (in_array($str, $this->ppe_note_skin->getNoteSkins(1))) return true;
+		$this->form_validation->set_message('_noteskin_exists', "Please choose a valid note skin.");
+		return false;
+	}
+	
+	// confirm the 4th note color is valid.
+	function _red_exists($str)
+	{
+		if (in_array($str, array(0, 1))) return true;
+		$this->form_validation->set_message('_red_exists', "Decide the color of the rhythm quarter notes.");
+		return false;
+	}
+	
+	// confirm the speed mod is valid.
+	function _speed_valid($str)
+	{
+		if (in_array($str, array(1, 2, 3, 4, 6, 8))) return true;
+		$this->form_validation->set_message('_speed_valid', 'A valid speed mod must be chosen.');
+		return false;
+	}
+	
+	// confirm the number of measures in each column is valid.
+	function _mpc_valid($str)
+	{
+		return in_array($str, array(4, 6, 8, 12, 16));
+	}
+	
+	// confirm the scale factor is valid.
+	function _scale_valid($str)
+	{
+		if (in_array($str, array(0.5, 0.75, 1, 1.25, 1.5, 1.75, 2))) return true;
+		$this->form_validation->set_message('_edit_exists', 'The scale chosen was not a valid scale.');
+		return false;
+	}
 	
 	function index()
 	{
@@ -98,57 +120,61 @@ class Chart extends Controller
 		$xml = $this->editcharter->genChart($notedata);
 		echo str_replace("xml:id", "id", $xml->saveXML());
 	}
-
-  function editProcess()
-  {
-    if ($this->form_validation->run() === FALSE)
-    {
-      $data['edits'] = $this->itg_edit_edit->getNonProblemEdits()->result_array();
-      $this->load->view('chart/editError', $data);
-      return;
-    }
-    $eid = $this->input->post('edits');
-    $path = sprintf("%sdata/itg_user_edits/itg_%06d.edit.gz", APPPATH, $eid);
-    if (!file_exists($path))
-    {
-      $data['edits'] = $this->itg_edit_edit->getNonProblemEdits()->result_array();
-      $this->load->view('chart/editError', $data);
-      return;
-    }
-    $this->load->model('itg_user_user');
-    $author = $this->itg_user_user->getUserByOldEditID($eid);
-    $this->load->library('EditParser');
-    $p = array('notes' => 1, 'strict_song' => 0, 'strict_edit' => 0);
-    $notedata = $this->editparser->get_stats(gzopen($path, "r"), $p);
-    $p = array('cols' => $notedata['cols'], 'kind' => $this->input->post('kind'),
-      'red4' => $this->input->post('red4'), 'speed_mod' => $this->input->post('speed'),
-      'mpcol' => $this->input->post('mpcol'), 'scale' => $this->input->post('scale'));
-    $this->load->library('EditCharter', $p);
-    $notedata['author'] = $author;
-    header("Content-Type: application/xhtml+xml");
-    $xml = $this->editcharter->genChart($notedata);
-    echo $xml->saveXML();
-  }
-  
-  // get the list of songs for possible chart previewing.
-  function songs()
-  {
-    $data['songs'] = $this->itg_song_song->getSongsWithGameAndDiff()->result_array();
-    $this->load->view('chart/songs', $data);
-  }
-  
-  // Use AJAJ to get the difficulties charted for each song.
-  function diff()
-  {
-    $sid = $this->uri->segment(3, false);
-    header("Content-type: application/json");
-    $path = "%sdata/itg_official/%d_%s.sm.gz";//, APPPATH, $sid);
-    foreach ($this->difficulties as $d)
-    {
-      $ret[$d] = file_exists(sprintf($path, APPPATH, $sid, $d));
-    }
-    echo json_encode($ret);
-  }
+	
+	function editProcess()
+	{
+		if ($this->form_validation->run() === FALSE)
+		{
+			$data['edits'] = $this->itg_edit_edit->getNonProblemEdits()->result_array();
+			$this->load->view('chart/editError', $data);
+			return;
+		}
+		
+		$url = sprintf("/chart/showEdit/%d/%s/%s/%s/%1.2f/%d/%1.2f",
+			$this->input->post('edits'),
+			$this->input->post('kind') ? $this->input->post('kind') : 'normal',
+			$this->input->post('red4') ? $this->input->post('red4') : 'red',
+			$this->input->post('noteskin') ? $this->input->post('noteskin') : 'original',
+			$this->input->post('speed'),
+			$this->input->post('mpcol'),
+			$this->input->post('scale')
+			);
+		redirect($url, 'location', 303);
+		return;
+	}
+	
+	// The code below is for implementing later.
+	function _showSongForm($first, $header)
+	{
+		$this->data['songs'] = $this->ppe_song_song->getSongsWithGameAndDiff()->result_array();
+		$this->data['form'] = array();
+		$this->data['form']['skin'] = $this->ppe_note_skin->getSelectSkins();
+		$this->data['form']['style'] = $this->ppe_note_style->getSelectStyles();
+		$this->_addJS('/js/official.js');
+		$this->_setHeader($header);
+		$this->_setTitle($header);
+		$this->_loadPage(array($first, 'chart/songForm'));
+	}
+	
+	// get the list of songs for possible chart previewing.
+	function songs()
+	{
+		$data['songs'] = $this->itg_song_song->getSongsWithGameAndDiff()->result_array();
+		$this->load->view('chart/songs', $data);
+	}
+	
+	// Use AJAJ to get the difficulties charted for each song.
+	function diff()
+	{
+		$sid = $this->uri->segment(3, false);
+		header("Content-type: application/json");
+		$path = "%sdata/itg_official/%d_%s.sm.gz";//, APPPATH, $sid);
+		foreach ($this->difficulties as $d)
+		{
+			$ret[$d] = file_exists(sprintf($path, APPPATH, $sid, $d));
+		}
+		echo json_encode($ret);
+	}
   
   function songProcess()
   {
