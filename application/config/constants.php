a4cbbc9ba59322e8f4ac962572cd6fb0bd88edb5
Jason Felds <jafelds@gmail.com>
2010-10-10 21:40:40 -0400

    Add the constant from the PPE side.

--- application/config/constants.php
+++ application/config/constants.php
@@ -38,6 +38,7 @@ define('FOPEN_READ_WRITE_CREATE_STRICT',		'x+b');
 
 
 // Constants relating to my own work.
+define('HELPERPATH', APPPATH . 'helpers/');
 define('APP_BASE_EDITS_PER_PAGE', 30);
 define('APP_MAX_EDIT_FILE_SIZE', 61440);
 define('APP_MIN_DIFFICULTY_RATING', 1);
