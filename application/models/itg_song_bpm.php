850e7aacb17c937dda64fc465b7ef3d62631580d
Jason Felds <jafelds@gmail.com>
2010-10-10 22:28:17 -0400

    Don't forget song BPMs and Stops.

--- application/models/itg_song_bpm.php
+++ application/models/itg_song_bpm.php
@@ -12,4 +12,15 @@ class itg_song_bpm extends Model
     return $this->db->where('song_id', $id)
       ->order_by('beat')->get('itg_song_bpm')->result();
   }
+  
+  // Get all of the BPM changes via the edit id.
+	public function getBPMsByEditID($id)
+	{
+		return $this->db->from('itg_song_bpm b')
+			->join('itg_edit_edit e', 'e.song_id = b.song_id')
+			->where('e.old_edit_id', $id)
+			->order_by('beat')
+			->get()
+			->result();
+	}
 }
--- application/models/itg_song_stop.php
+++ application/models/itg_song_stop.php
@@ -12,4 +12,15 @@ class itg_song_stop extends Model
     return $this->db->where('song_id', $id)
       ->order_by('beat')->get('itg_song_stop')->result();
   }
+  
+  // Get all of the BPM stops via the edit id.
+	public function getStopsByEditID($id)
+	{
+		return $this->db->from('itg_song_stop s')
+			->join('itg_edit_edit e', 'e.song_id = s.song_id')
+			->where('e.old_edit_id', $id)
+			->order_by('beat')
+			->get()
+			->result();
+	}
 }
