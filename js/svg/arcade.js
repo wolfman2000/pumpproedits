d7d2e47c5d8120d74bacdc8964b9a2bf747fa62d
Jason Felds <jafelds@gmail.com>
2010-10-19 17:07:13 -0400

    The start of pulsing lifts for the arcade skin.

--- js/svg/arcade.js
+++ js/svg/arcade.js
@@ -1,23 +1,36 @@
 var iter = 0;
+var timer = 400;
+var centering = 8;
 
 function animateLifts(lifts)
 {
 	lifts.each(function(){
-		if (iter % 2)
+		var x = parseFloat($(this).attr('x'));
+		var y = parseFloat($(this).attr('y'));
+		var trans = 'scale(SCALE)';
+		
+		if (!(iter % 2))
 		{
-			$(this).attr('x', parseFloat($(this).attr('x')) + 16);
+			trans = trans.replace('SCALE', '0.5');
+			$(this).attr('x', x * 2 + centering);
+			$(this).attr('y', y * 2 + centering);
+			//$(this).attr('x', parseFloat($(this).attr('x')) + 16);
 		}
 		else
 		{
-			$(this).attr('x', parseFloat($(this).attr('x')) - 16);
+			trans = trans.replace('SCALE', '1');
+			$(this).attr('x', (x - centering) / 2);
+			$(this).attr('y', (y - centering) / 2);
+			//$(this).attr('x', parseFloat($(this).attr('x')) - 16);
 		}
+		$(this).attr('transform', trans);
 	});
 	iter++;
 }
 
 $(document).ready(function()
 {
-	$("use[name=lift]").doTimeout(400, function(){
+	$("use[name=lift]").doTimeout(timer, function(){
 		animateLifts($(this));
 		return true;
 	});
